4bc297fda776ade94417603eabe915efa95eb2ee
leultewolde <leulwoldegabriel@gmail.com>
2025-06-08 02:30:52 -0400

    Setup backend

--- dist/routes/auth.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { login, signup } from '../controllers/auth.js';
-const router = Router();
-router.post('/signup', signup);
-router.post('/login', login);
-export default router;
--- dist/routes/tree.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getTree, addMember } from '../controllers/tree.js';
-import { authenticate } from '../middleware/auth.js';
-const router = Router();
-router.use(authenticate);
-router.get('/', getTree);
-router.post('/', addMember);
-export default router;
